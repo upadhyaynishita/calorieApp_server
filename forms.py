c4b1ebcc36972c3a1ab6bddc1e0cf13bd0f04e4b
Atharva Patil <atharvapatil@Atharvas-MBP.lan>
2021-11-10 12:47:27 -0500

    Fixing food query

--- forms.py
+++ forms.py
@@ -41,6 +41,17 @@ class Food:
         app = App()
         mongo = app.mongo
 
+        f = open('food_data/calories.csv', 'r', encoding = "ISO-8859-1")
+        l = f.readlines()
+
+        for i in range(1, len(l)):
+            l[i] = l[i][1:len(l[i]) - 2]
+
+        for i in range(1, len(l)):
+            temp = l[i].split(",")
+            mongo.db.food.insert({'food': temp[0], 'calories': temp[1]})
+
+
         cursor = mongo.db.food.find()
         get_docs = []
         for record in cursor:
